84f2599d865ee39f21ac418ab665ea83bc1d05e4
Ferenc Merics <mericsferenc@gmail.com>
2025-09-10 18:29:20 +0200

    homepage

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import AdminPage from "../pages/AdminPage";
 import TeacherPage from "../pages/TeacherPage";
 import StudentPage from "../pages/StudentPage";
 import ProfilePage from "../pages/ProfilePage";
-import Navbar from "../pages/Navbar";
 import ProtectedRoute from "../pages/ProtectedRoute";
+import HomePage from "../pages/HomePage";
+// import HomePage from "../components/homepage/homePage";
 
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<ProtectedRoute allowedRoles={["STUDENT","TEACHER","ADMIN"]}><ProfilePage /></ProtectedRoute>} />
         <Route path="/student" element={<ProtectedRoute allowedRoles={["STUDENT"]}><StudentPage /></ProtectedRoute>} />
